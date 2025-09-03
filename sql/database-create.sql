5a74d9a4122ad01f5a29d8b177be1946299513b5
JoBioux <jonas.beux@gmail.com>
2025-09-03 14:59:58 +0200

    #3 : fixed my last commit's error ; thought id should be changed in all tables but I was wrong. I also added a template for future adding of references using foreign keys.

--- sql/database-create.sql
+++ sql/database-create.sql
@@ -1,65 +1,67 @@
 create table bddFilms.langue (
-    id INT (9) NOT NULL AUTO_INCREMENT,
+    id INT (9) PRIMARY KEY AUTO_INCREMENT,
     libelle CHARACTER (16) NOT NULL,
-    abr     CHARACTER (4) NOT NULL  
+    abr     CHARACTER (4) NOT NULL 
+
 );
 
 create table bddFilms.realisateur (
-    id INT (9) NOT NULL AUTO_INCREMENT,
+    id INT (9) PRIMARY KEY AUTO_INCREMENT,
     nom     CHARACTER (32) NOT NULL,
     prenom  CHARACTER (32) NOT NULL
 );
 
 create table bddFilms.acteur (
-    id INT (9) NOT NULL AUTO_INCREMENT,
+    id INT (9) PRIMARY KEY AUTO_INCREMENT,
     nom     CHARACTER (32) NOT NULL,
     prenom  CHARACTER (32) NOT NULL
 );
 
 create table bddFilms.genre (
-    id INT (9) NOT NULL AUTO_INCREMENT,
+    id INT (9) PRIMARY KEY AUTO_INCREMENT,
     libelle CHARACTER (16) NOT NULL
 );
 
 create table bddFilms.metrage (
-    id INT (9) NOT NULL AUTO_INCREMENT,
+    id INT (9) PRIMARY KEY AUTO_INCREMENT,
     libelle CHARACTER (16) NOT NULL
 );
 
 create table bddFilms.film (
-    id INT (9) NOT NULL AUTO_INCREMENT,
+    id INT (9) PRIMARY KEY AUTO_INCREMENT,
     titreFR         CHARACTER (64) NOT NULL,
     sous-titreFR    CHARACTER (32),
     annee           CHARACTER (4)  NOT NULL,
-    id_realisateur  INT (9)   
+    id_realisateur  INT (9),
+    FOREIGN KEY (id_realisateur) REFERENCES bddFilms.realisateur (id) 
 );
 
 create table bddFilms.serie (
-    id_serie INT (9) NOT NULL AUTO_INCREMENT,
+    id INT (9) PRIMARY KEY AUTO_INCREMENT,
     titreFR     CHARACTER (32) NOT NULL,
     annee_debut CHARACTER (4)  NOT NULL,
-    annee_fin   CHARACTER (4)
+    annee_fin   CHARACTER (4)id INT (9) 
 );
 
 create table bddFilms.saison (
-    id_saison INT (9) NOT NULL AUTO_INCREMENT,
+    id INT (9) PRIMARY KEY AUTO_INCREMENT,
     titreFR     CHARACTER (32),
     numero      INT (2)
 );
 
 create table bddFilms.episode (
-    id_episode INT (9) NOT NULL AUTO_INCREMENT,
+    id INT (9) PRIMARY KEY AUTO_INCREMENT,
     titreFR     CHARACTER (32) NOT NULL,
     numero      INT (2)  
 );
 
 create table bddFilms.indexRecherche (
-    id_indexRecherche INT (9) NOT NULL AUTO_INCREMENT,
+    id INT (9) PRIMARY KEY AUTO_INCREMENT,
     titreVO     CHARACTER (32) NOT NULL
 );
 
 create table bddFilms.video (
-    id_video INT (9) NOT NULL AUTO_INCREMENT,
+    id INT (9) PRIMARY KEY AUTO_INCREMENT,
     qualite     CHARACTER (32) NOT NULL,
     duree       INT (2),
     sous-titres BOOLEAN NOT NULL DEFAULT TRUE,
@@ -75,30 +77,40 @@ create table bddFilms.video (
 create table bddFilms.film_apour_langue (
     id_film     INT (9) NOT NULL,
     id_langue   INT (9) NOT NULL,
-    CONSTRAINT id PRIMARY KEY (id_film,id_langue)
+    CONSTRAINT id PRIMARY KEY (id_film,id_langue),
+    FOREIGN KEY (id_film) REFERENCES bddFilms.film (id),
+    FOREIGN KEY (id_langue) REFERENCES bddFilms.langue (id)
 );
 
 create table bddFilms.serie_apour_langue (
     id_serie     INT (9) NOT NULL,
     id_langue   INT (9) NOT NULL,
-    CONSTRAINT id PRIMARY KEY (id_serie,id_langue)
-);
-
-create table bddFilms.template (
-    id_template     INT (9) NOT NULL,
-    id_template   INT (9) NOT NULL,
-    CONSTRAINT id PRIMARY KEY (id_template,id_template)
+    CONSTRAINT id PRIMARY KEY (id_serie,id_langue),
+    FOREIGN KEY (id_serie) REFERENCES bddFilms.serie (id),
+    FOREIGN KEY (id_langue) REFERENCES bddFilms.langue (id)
 );
 
 
 
 
+create table bddFilms.template (
+    id_templateA     INT (9) NOT NULL,
+    id_templateB   INT (9) NOT NULL,
+    CONSTRAINT id PRIMARY KEY (id_templateA,id_templateB),
+    FOREIGN KEY (id_templateA) REFERENCES bddFilms.templateA (id),
+    FOREIGN KEY (id_templateB) REFERENCES bddFilms.templateB (id)
+);
+
+
+
 
 
 
 
 create table bddFilms.template (
-    id_template     INT (9) NOT NULL,
-    id_template   INT (9) NOT NULL,
-    CONSTRAINT id PRIMARY KEY (id_template,id_template)
+    id_templateA     INT (9) NOT NULL,
+    id_templateB   INT (9) NOT NULL,
+    CONSTRAINT id PRIMARY KEY (id_templateA,id_templateB),
+    FOREIGN KEY (id_templateA) REFERENCES bddFilms.templateA (id),
+    FOREIGN KEY (id_templateB) REFERENCES bddFilms.templateB (id)
 );
