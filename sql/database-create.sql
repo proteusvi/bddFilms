c51bf57be048d1f340b50abd1d1ca3d2d1b7ba13
JoBioux <jonas.beux@gmail.com>
2025-09-03 14:15:19 +0200

    fix: #3 updated usage of fk_abc to id_abc in all relevant tables.

--- sql/database-create.sql
+++ sql/database-create.sql
@@ -31,36 +31,35 @@ create table bddFilms.film (
     titreFR         CHARACTER (64) NOT NULL,
     sous-titreFR    CHARACTER (32),
     annee           CHARACTER (4)  NOT NULL,
-    fk_realisateur  INT (9)   
+    id_realisateur  INT (9)   
 );
 
 create table bddFilms.serie (
-    id INT (9) NOT NULL AUTO_INCREMENT,
+    id_serie INT (9) NOT NULL AUTO_INCREMENT,
     titreFR     CHARACTER (32) NOT NULL,
-    --saison      INT (2),
     annee_debut CHARACTER (4)  NOT NULL,
     annee_fin   CHARACTER (4)
 );
 
 create table bddFilms.saison (
-    id INT (9) NOT NULL AUTO_INCREMENT,
+    id_saison INT (9) NOT NULL AUTO_INCREMENT,
     titreFR     CHARACTER (32),
     numero      INT (2)
 );
 
 create table bddFilms.episode (
-    id INT (9) NOT NULL AUTO_INCREMENT,
+    id_episode INT (9) NOT NULL AUTO_INCREMENT,
     titreFR     CHARACTER (32) NOT NULL,
     numero      INT (2)  
 );
 
 create table bddFilms.indexRecherche (
-    id INT (9) NOT NULL AUTO_INCREMENT,
+    id_indexRecherche INT (9) NOT NULL AUTO_INCREMENT,
     titreVO     CHARACTER (32) NOT NULL
 );
 
 create table bddFilms.video (
-    id INT (9) NOT NULL AUTO_INCREMENT,
+    id_video INT (9) NOT NULL AUTO_INCREMENT,
     qualite     CHARACTER (32) NOT NULL,
     duree       INT (2),
     sous-titres BOOLEAN NOT NULL DEFAULT TRUE,
@@ -74,21 +73,21 @@ create table bddFilms.video (
 
 
 create table bddFilms.film_apour_langue (
-    fk_film     INT (9) NOT NULL,
-    fk_langue   INT (9) NOT NULL,
-    CONSTRAINT id PRIMARY KEY (fk_film,fk_langue)
+    id_film     INT (9) NOT NULL,
+    id_langue   INT (9) NOT NULL,
+    CONSTRAINT id PRIMARY KEY (id_film,id_langue)
 );
 
 create table bddFilms.serie_apour_langue (
-    fk_serie     INT (9) NOT NULL,
-    fk_langue   INT (9) NOT NULL,
-    CONSTRAINT id PRIMARY KEY (fk_serie,fk_langue)
+    id_serie     INT (9) NOT NULL,
+    id_langue   INT (9) NOT NULL,
+    CONSTRAINT id PRIMARY KEY (id_serie,id_langue)
 );
 
 create table bddFilms.template (
-    fk_template     INT (9) NOT NULL,
-    fk_template   INT (9) NOT NULL,
-    CONSTRAINT id PRIMARY KEY (fk_template,fk_template)
+    id_template     INT (9) NOT NULL,
+    id_template   INT (9) NOT NULL,
+    CONSTRAINT id PRIMARY KEY (id_template,id_template)
 );
 
 
@@ -99,7 +98,7 @@ create table bddFilms.template (
 
 
 create table bddFilms.template (
-    fk_template     INT (9) NOT NULL,
-    fk_template   INT (9) NOT NULL,
-    CONSTRAINT id PRIMARY KEY (fk_template,fk_template)
+    id_template     INT (9) NOT NULL,
+    id_template   INT (9) NOT NULL,
+    CONSTRAINT id PRIMARY KEY (id_template,id_template)
 );
