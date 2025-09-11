48a72ecd785e696ef1e6e9aae696e337899f488a
Didier <dbeux@infomaniak.ch>
2025-09-11 13:56:23 +0200

    #3 fix: delete duplicate table and move serie before realisateur for constraint.

--- sql/drop_tables.sql
+++ sql/drop_tables.sql
@@ -9,12 +9,11 @@ drop table bddFilms.serie_apour_genre;
 drop table bddFilms.langue;
 drop table bddFilms.acteur;
 drop table bddFilms.genre;
-drop table bddFilms.metrage;
 drop table bddFilms.episode;
 drop table bddFilms.video;
 drop table bddFilms.film;
+drop table bddFilms.serie;
 drop table bddFilms.realisateur;
 drop table bddFilms.metrage;
-drop table bddFilms.serie;
 
 commit;
