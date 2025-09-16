95be25c86ca524779da4f9315ff7ab1afad104c2
Didier <dbeux@infomaniak.ch>
2025-09-16 14:43:29 +0200

    #4 chore: Analysing different bash techno to be used for bddfilm implementation.

---
+++ src/script/UiInterfaceLibrary.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Library to be used in external scripts.
+# @auth  : proteusvi@gmail.com
+# @since : mar. 16 sept. 2025 14:40:49 CEST
+
+#+------------------------------------+
+#| Display title passed by parameter. |
+#| Parameter : $title (string).       |
+#| Return    : Title formated.        |
+#+------------------------------------+
+function displayTitle() {
+    title="${1}"
+    echo -en "+---------------------------------+\n"
+    echo -en "# ${title}$(createSpacesForTitle "${title}")#\n"
+    echo -en "+---------------------------------+\n"
+}
+
+
+#+------------------------------------+
+#| Display spaces needed after text   |
+#|   passed by parameter.             |
+#| Parameter : $text (string)         |
+#| Return    : Spaces needed.         |
+#+------------------------------------+
+function createSpacesForTitle() {
+    maxSpaces=$((33))
+    text="$1"
+    length=${#text}
+    spaces='                                 '
+    if [[ "${length}" > "0" ]]; then
+       i=$((${maxSpaces}-${length}-1))
+       spaces=''
+       while [ $i != 0 ]; do
+          spaces=${spaces}' '
+          ((i--)) 
+       done
+    fi
+    echo -en "${spaces}";
+}
+
---
+++ tests/unit/DisplayTitle.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Load the library.
+. ../../src/script/src/UiInterfaceLibrary.sh
+
+# main
+displayTitle "Create film"
+displayTitle "Create serie"
+displayTitle "Create season"
+displayTitle "Create episode"
+displayTitle "Didier Beux"
