719bdacdc06b1886f1d23bfb747a3a6ddb3d3bca
Didier <dbeux@infomaniak.ch>
2025-09-18 09:49:58 +0200

    #4 test: Add test on spaces for title creation.

---
+++ tests/unit/testCreateSpacesForTitle.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#    .--.
+#   |o_o |
+#   |:_/ |
+#  //   \ \
+# (|     | )
+# /'\_   _/`\
+# \___)=(___/
+#Test file to create spaces after a text in a limited number of spaces autorised.
+# @auth  : proteusvi@gmail.com
+# @since : mer. 17 sept. 2025 08:34:50 CEST
+
+# Load the libraries.
+. src/script/UiInterfaceLibrary.sh
+. tests/unit/DisplayResultTestLibrary.sh
+
+displayResultTest "Test createSpacesForTitle 1 : Normal string." "         " "$(createSpacesForTitle 'Didier Beux and family.')"
+
+displayResultTest "Test createSpacesForTitle 2 : Empty parameter." "                                " "$(createSpacesForTitle)" 
+
+toCompare="\e[31mError :\e[0m\n"
+toCompare="${toCompare}Title is equal or greater than 32 characters."
+displayErrorMessageTest "Test createSpacesForTitle 3 : More than 32 character." \
+"$(echo -en ${toCompare})" \
+"$(createSpacesForTitle 'This phrase is greater than 32 characters.')"
